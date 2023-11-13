0a5825419cae8f3c232284e8bc42f2b0bb8c6c11
Adegoke Surajudeen <surajudeenadegoke@gmail.com>
2023-11-13 12:03:49 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-function App() {
+import React, { useState } from "react";
+import { numbers, lowerCase, upperCase, symbols } from "./characters";
+const App = () => {
+  const [password, setPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState("");
+  const [includeUpperCase, setIncludeUpperCase] = useState(false);
+  const [includeLowerCase, setIncludeLowerCase] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+
+  const handleGeneratePassword = () => {
+    let characterList = "";
+    if (includeUpperCase) {
+      characterList += upperCase;
+    }
+    if (includeLowerCase) {
+      characterList += lowerCase;
+    }
+    if (includeNumbers) {
+      characterList += numbers;
+    }
+    if (includeSymbols) {
+      characterList += symbols;
+    }
+    
+    const generatePassword = ()=>{
+      console.log (passwordLength);
+      console.log (characterList.length);
+
+      let password = "";
+      for(let i=0; i<passwordLength; i++) {
+        let characterIndex = Math.round(Math.random() * characterList.length);
+        console.log(characterList.charAt(characterIndex));
+        password +=characterList.charAt(characterIndex);
+      }
+      return password
+    }
+    setPassword(generatePassword());
+  };
+  console.log(password);
+
   return (
     <div className="App">
       <div className="container">
         <div className="generator">
-          <h1>Password Generator</h1>
+          <h1 className="generator-header">Password Generator</h1>
           <div className="generate-password">
-            <h3> password</h3>
+            <h3 className="password"> {password}</h3>
             <button className="copy-btn">
               <i className="far fa-clipboard"></i>
             </button>
           </div>
           <div className="form-group">
-            <label htmlFor="password-strength">Password Strength</label>
+            <label htmlFor="password-strength">Password Length</label>
             <input
               type="number"
               id="password-strength"
               name="password-strength"
               max="20"
               min="10"
+              defaultValue={20}
+              onChange={(e) => setPasswordLength(e.target.value)}
             />
           </div>
           <div className="form-group">
@@ -26,6 +68,8 @@ function App() {
               type="checkbox"
               id="uppercase-letters"
               name="uppercase-letters"
+              checked={includeUpperCase}
+              onChange={(e) => setIncludeUpperCase(e.target.checked)}
             />
           </div>
           <div className="form-group">
@@ -34,6 +78,8 @@ function App() {
               type="checkbox"
               id="lowercase-letters"
               name="lowercase-letters"
+              checked={includeLowerCase}
+              onChange={(e) => setIncludeLowerCase(e.target.checked)}
             />
           </div>
           <div className="form-group">
@@ -42,6 +88,8 @@ function App() {
               type="checkbox"
               id="include-numbers"
               name="include-numbers"
+              checked={includeNumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
             />
           </div>
           <div className="form-group">
@@ -50,13 +98,20 @@ function App() {
               type="checkbox"
               id="include-symbols"
               name="include-symbols"
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
             />
           </div>
-          <button className="generate-btn">Generate Button</button>
+          <button className="generate-btn" onClick={handleGeneratePassword}>
+            Generate Button
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
---
+++ src/characters.js
@@ -0,0 +1,4 @@
+export const numbers = '0123456789';
+export const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+export const symbols = '!@#$%&^*+-' 
